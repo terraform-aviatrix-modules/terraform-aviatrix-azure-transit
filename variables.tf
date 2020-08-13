eca78400597ad28194df545b51b4ba20f745ece3
Dennizz <root@ipaddr.nl>
2020-08-13 12:40:45 +0200

    Formatting

--- variables.tf
+++ variables.tf
@@ -1,26 +1,26 @@
 variable "region" {
-    description = "The Azure region to deploy this module in"
-    type = string
+  description = "The Azure region to deploy this module in"
+  type        = string
 }
 
 variable "cidr" {
-    description = "The CIDR range to be used for the VNET"
-    type = string
+  description = "The CIDR range to be used for the VNET"
+  type        = string
 }
 
 variable "azure_account_name" {
-    description = "The Azure account name, as known by the Aviatrix controller"
-    type = string
+  description = "The Azure account name, as known by the Aviatrix controller"
+  type        = string
 }
 
 variable "instance_size" {
-    description = "Azure Instance size for the Aviatrix gateways"
-    type = string
-    default = "Standard_B1ms"
+  description = "Azure Instance size for the Aviatrix gateways"
+  type        = string
+  default     = "Standard_B1ms"
 }
 
 variable "ha_gw" {
-    description = "Boolean to determine if module will be deployed in HA or single mode"
-    type = bool
-    default = true
+  description = "Boolean to determine if module will be deployed in HA or single mode"
+  type        = bool
+  default     = true
 }
